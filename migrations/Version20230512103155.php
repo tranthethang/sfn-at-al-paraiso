701dc9eea6c560c2df627ce0befd738b01db8886
tranthethang <tranthethang@gmail.com>
2023-05-15 09:01:34 +0700

    add migration for entity: room-type

--- migrations/Version20230512103155.php
+++ migrations/Version20230512103155.php
@@ -20,7 +20,7 @@ final class Version20230512103155 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE hotel (id INT AUTO_INCREMENT NOT NULL, category_id_id INT DEFAULT NULL, hotel_name VARCHAR(255) NOT NULL, description VARCHAR(255) DEFAULT NULL, is_active TINYINT(1) NOT NULL, created_at DATETIME NOT NULL, updated_at DATETIME NOT NULL, INDEX IDX_3535ED99777D11E (category_id_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE hotel (id INT AUTO_INCREMENT NOT NULL, category_id INT DEFAULT NULL, hotel_name VARCHAR(255) NOT NULL, description VARCHAR(255) DEFAULT NULL, is_active TINYINT(1) NOT NULL, created_at DATETIME NOT NULL, updated_at DATETIME NOT NULL, INDEX IDX_3535ED99777D11E (category_id_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE hotel ADD CONSTRAINT FK_3535ED99777D11E FOREIGN KEY (category_id_id) REFERENCES category (id)');
     }
 
